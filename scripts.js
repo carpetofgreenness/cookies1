ea701e1efa32b90f845d380d85ba722839870abb
Hannah Weinstein <hannahjweinstein@gmail.com>
2017-01-19 17:34:07 -0500

    created second page that leaves cookie so user sees animation only once

--- scripts.js
+++ scripts.js
@@ -6,7 +6,27 @@ $("#submit").click(function() {
 
 if (Cookies.get("Name") == null) {
 	$("#welcome").text("Wecome to our page that you are visiting for the first time")
+	$("#welcome2").text("You're seeing this for the first time!")
+	$("#welcome2").animate( { "font-size" : "5em" } )
 } else {
 	var name = Cookies.get("Name")
-	$("#welcome").text("hello "+name+", you are visiting this site agian.")
+	$("#welcome").text("hello "+name+", you are visting this site agian.")
+	$("#welcome2").text("hello "+name+", you have been here before. We dont' care.")
+}
+
+
+//second page
+if (Cookies.get("HasVisited") == null) {
+	$("#welcome2").text("Hi "+Cookies.get("Name")+"! You're seeing this for the first time!")
+	$("#welcome2").animate(
+		{ "font-size" : "5em" },
+		function complete() {
+			Cookies.set("HasVisited",true);
+			console.log("you have visited this page");
+		}
+	)
+} else {
+	var name = Cookies.get("Name")
+	$("#welcome").text("hello "+name+", you are visting this site agian.")
+	$("#welcome2").text("hello "+name+", you have been here before. We dont' care.")
 }
